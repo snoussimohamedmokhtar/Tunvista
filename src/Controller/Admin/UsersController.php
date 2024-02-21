5033d195486892645b3d8fc2d99a544eee6c07bf
Anis Chebbi <97995627+chebbianis@users.noreply.github.com>
2024-02-21 01:52:05 +0100

    delete and add user done

--- src/Controller/Admin/UsersController.php
+++ src/Controller/Admin/UsersController.php
@@ -2,20 +2,96 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\User;
+use App\Form\RegistrationFormType;
+use App\Form\UserFormType;
+use App\Form\UserUpdateFormType;
 use App\Repository\UserRepository;
+use App\Security\UserAuthenticator;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\UserAuthenticatorInterface;
 
 #[Route('/admin/Users',name:'admin_users_')]
 class UsersController extends AbstractController
 {
 
-    #[Route('/',name: 'index')]
-    public function index(UserRepository $userRepository):Response{
-        $users = $userRepository->findBy( [] ,[
+    #[Route('/', name: 'index')]
+    public function index(UserRepository $userRepository): Response
+    {
+        $users = $userRepository->findBy([], [
             'firstName' => 'asc'
         ]);
         return $this->render('admin/users/index.html.twig', compact('users'));
+    }
+
+    #[Route('/new', name: 'admin_users_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, UserPasswordHasherInterface $userPasswordHasher, UserAuthenticatorInterface $userAuthenticator, UserAuthenticator $authenticator, EntityManagerInterface $entityManager): Response
+    {
+        $user = new User();
+        $form = $this->createForm(UserFormType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // encode the plain password
+            $user->setPassword(
+                $userPasswordHasher->hashPassword(
+                    $user,
+                    $form->get('plainPassword')->getData()
+                )
+            );
+
+            $entityManager->persist($user);
+            $entityManager->flush();
+            // do anything else you need here, like send an email
+
+            return $this->redirectToRoute('admin_users_index');
+
+        }
+        return $this->render('admin/users/new.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'admin_users_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, User $user, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(UserFormType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('admin_users_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('admin/users/edit.html.twig', [
+            'user' => $user,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'admin_users_delete', methods: ['POST'])]
+    public function delete(Request $request, int $id, EntityManagerInterface $entityManager): Response
+    {
+        // Charger l'utilisateur à supprimer depuis la base de données
+        $user = $entityManager->getRepository(User::class)->find($id);
+
+        // Vérifier si l'utilisateur existe
+        if (!$user) {
+            throw $this->createNotFoundException('User not found');
+        }
+
+        // Vérifier le jeton CSRF
+        if ($this->isCsrfTokenValid('delete' . $user->getId(), $request->request->get('_token'))) {
+            $entityManager->remove($user);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('admin_users_index', [], Response::HTTP_SEE_OTHER);
     }
 }
---
+++ src/Form/UserFormType.php
@@ -0,0 +1,147 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Region;
+use App\Entity\User;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Email;
+use Symfony\Component\Validator\Constraints\IsTrue;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Regex;
+
+class UserFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('email', EmailType::class, [
+                'attr' => [
+                    'class' => 'form-control'
+                ],
+                'label' => 'E-mail',
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Please enter an email address',
+                    ]),
+                    new Email([
+                        'message' => 'The email "{{ value }}" is not a valid email.',
+                    ]),
+                ]
+            ])
+            ->add('lastName', TextType::class, [
+                'attr' => [
+                    'class' => 'form-control'
+                ],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Please enter your last name',
+                    ]),
+                    new Length([
+                        'min' => 2,
+                        'max' => 255,
+                        'minMessage' => 'Your last name must be at least {{ limit }} characters long',
+                        'maxMessage' => 'Your last name cannot be longer than {{ limit }} characters',
+                    ]),
+                ]
+            ])
+            ->add('firstName', TextType::class, [
+                'attr' => [
+                    'class' => 'form-control'
+                ],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Please enter your first name',
+                    ]),
+                    new Length([
+                        'min' => 2,
+                        'max' => 255,
+                        'minMessage' => 'Your first name must be at least {{ limit }} characters long',
+                        'maxMessage' => 'Your first name cannot be longer than {{ limit }} characters',
+                    ]),
+                ]
+            ])
+            ->add('region', EntityType::class, [
+                'class' => Region::class,
+                'placeholder' => 'Choisir une région',
+                'choice_label' => 'nom',
+                'attr' => [
+                    'class' => 'form-control'
+                ]
+            ])
+            ->add('ville', TextType::class, [
+                'attr' => [
+                    'class' => 'form-control'
+                ],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Please enter your city',
+                    ]),
+                    new Length([
+                        'min' => 2,
+                        'max' => 255,
+                        'minMessage' => 'Your city name must be at least {{ limit }} characters long',
+                        'maxMessage' => 'Your city name cannot be longer than {{ limit }} characters',
+                    ]),
+                ]
+            ])
+            ->add('adresse', TextType::class, [
+                'attr' => [
+                    'class' => 'form-control'
+                ],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Please enter your address',
+                    ]),
+                    new Length([
+                        'min' => 2,
+                        'max' => 255,
+                        'minMessage' => 'Your address must be at least {{ limit }} characters long',
+                        'maxMessage' => 'Your address cannot be longer than {{ limit }} characters',
+                    ]),
+                ]
+            ])
+            ->add('agreeTerms', CheckboxType::class, [
+                'mapped' => false,
+                'constraints' => [
+                    new IsTrue([
+                        'message' => 'You should agree to our terms.',
+                    ]),
+                ],
+            ])
+            ->add('plainPassword', PasswordType::class, [
+                // instead of being set onto the object directly,
+                // this is read and encoded in the controller
+                'mapped' => false,
+
+                'attr' => [
+                    'autocomplete' => 'new-password',
+                    'class' => 'form-control'
+                ],
+
+                'constraints' => [
+                    new Regex('~^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$~',
+                        "Password should be Minimum eight in length One Upper case, one lower case, one digit, one special character"
+
+                    ),
+                ],
+            ])
+        ;
+    }
+
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
